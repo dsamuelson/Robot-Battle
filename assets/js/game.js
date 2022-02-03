dc37b6ae541160031e96ead1380f04e0c3e22cd0
David Samuelson <linksn.1fan@gmail.com>
2022-02-03 11:32:44 -0700

    Initial Round complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,37 @@
-// This creates a function named "fight"
+let playerName = window.prompt("What's your Name?");
+let playerHealth = 100;
+let playerAttack = 10;
 
-fight();
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
 
-function fight() {
+window.alert("Welcome to Robot Gladiators!");
+
+let fight = function () {
     
+    if (playerHealth > 0) {
+        enemyHealth = enemyHealth - playerAttack;
+
+        window.alert (playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " HP remaining!");
+    }
+
+    if (enemyHealth > 0){
+        playerHealth = playerHealth - enemyAttack;
+
+    window.alert (enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " HP remaining!");
+    }
+
+}
+
+while (playerHealth > 0 && enemyHealth > 0) {
+    fight ();
+};
+
+if (enemyHealth <=0) {
+    window.alert(enemyName + " has been knocked out");
+}
+
+if (playerHealth <=0) {
+    window.alert("You have been knocked out! GAME OVER!")
 }
